1cab94c0a293355141c981fa8ea748f4ad26137d
sintyhrith <161000418+sintyhrith@users.noreply.github.com>
2026-02-14 16:51:35 -0600

    Update players.js

--- players.js
+++ players.js
@@ -1,6 +1,7 @@
 const defaultPlayers = [
-  { name: "Virat Kohli", title: "Batsman", badge: "C", img: "https://github.com/sintyhrith/NASHIMAGE/blob/main/images/players/rsharma.png" },
+  { name: "Virat Kohli", title: "Batsman", badge: "C", img: "https://raw.githubusercontent.com/sintyhrith/NASHIMAGE/main/images/players/rsharma.png" },
   { name: "Rohit Sharma", title: "Batsman", img: "https://raw.githubusercontent.com/YourUsername/cricket-team/main/images/players/rohit.png" },
   { name: "MS Dhoni", title: "Wicket Keeper", badge: "WK", img: "https://raw.githubusercontent.com/YourUsername/cricket-team/main/images/players/dhoni.png" }
 ];
+
 
